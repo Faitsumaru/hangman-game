ccdbb52c31cd5d0a772a6ba7e89e09b96edc9f3b
Faitsumaru <kgm20011505@mail.ru>
2023-01-23 16:34:29 +0300

    Hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
@@ -7,9 +8,20 @@ function App() {
   });
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  console.log(wordToGuess);
-
-  return <h1>{wordToGuess}</h1>
+  // console.log(wordToGuess);
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{ fontSize: "2rem",  textAlign: "center" }}>Lose Win</div>
+        <HangmanDrawing />
+    </div>
+  )
 }
 
 export default App
