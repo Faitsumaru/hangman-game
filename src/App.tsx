c848f6ba9e1db30bf0b5cf9698bc0aba8cd5bb9d
Faitsumaru <kgm20011505@mail.ru>
2023-01-28 14:22:49 +0300

    victory fireworks

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,60 @@ import words from "./wordList.json";
 import { HangmanDrawing } from "./HangmanDrawing";
 import { HangmanWord } from "./HangmanWord";
 import { Keyboard } from "./Keyboard";
+import { Fireworks } from 'fireworks-js';
+
+//fireworks
+const app  = document.querySelector('html')
+const fireworks = new Fireworks(app as any, {
+  autoresize: true,
+  opacity: 0.5,
+  acceleration: 1.05,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 50,
+  traceLength: 3,
+  traceSpeed: 10,
+  explosion: 5,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round',
+  hue: {
+    min: 0,
+    max: 360
+  },
+  delay: {
+    min: 30,
+    max: 60
+  },
+  rocketsPoint: {
+    min: 50,
+    max: 50
+  },
+  lineWidth: {
+    explosion: {
+      min: 1,
+      max: 3
+    },
+    trace: {
+      min: 1,
+      max: 2
+    }
+  },
+  brightness: {
+    min: 50,
+    max: 80
+  },
+  decay: {
+    min: 0.015,
+    max: 0.03
+  },
+  mouse: {
+    click: false,
+    move: false,
+    max: 1
+  }
+})
+//-//
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)]
@@ -59,6 +113,12 @@ function App() {
       document.removeEventListener("keypress", handler)
     }
   }, [])
+
+  useEffect(() => {
+    if (isWinner)
+      return fireworks.start()
+    fireworks.stop()
+  })
 
   // console.log(wordToGuess);
   return (
